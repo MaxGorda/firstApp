5ba019b45977313e2301fa26343d2300e0274c3e
Max (macbook 16) <maxlukru@gmail.com>
2023-01-19 21:26:15 +0200

    20. Условия

--- firstApp/script.js
+++ firstApp/script.js
@@ -1,40 +1,46 @@
 'use strict';
 
-/* Задание на урок:
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-Проверить, чтобы все работало без ошибок в консоли */
+ if (4 == 9) {
+    console.log('Bingo!');
+ } else {
+    console.log('Error!');
+ }
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+///////////////////////////////////////////
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+ const num = 100;
 
- const a = prompt('Один из последних просмотренных фильмов?', ''),
-       b = prompt('На сколько оцените его?', ''),
-       c = prompt('Один из последних просмотренных фильмов?', ''),
-       d = prompt('На сколько оцените его?', '');
+ if (num < 49){
+    console.log('Nope!');
+ } else if (num > 100) {
+    console.log('Its too much bro!')
+ } else {
+    console.log ('You are right!')
+ }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+///////////////////////////////////////////
 
-console.log(personalMovieDB);
+ const num2 = 51;
+
+ (num2 === 50) ? console.log ('You are right!') : console.log('Nope!');
+
+ ///////////////////////////////////////////
+
+ const num3 = 51
+
+ switch (num3) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('Получилось');
+        break;
+    default:
+        console.log('Попробуй еще разок');
+        break;
+ }
+ 
+ 
