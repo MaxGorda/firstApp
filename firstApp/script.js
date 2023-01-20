84560d3dd7bfd67af84e6f355fe14783d3019218
Max (macbook 16) <maxlukru@gmail.com>
2023-01-20 12:54:28 +0200

    21. Логические операторы

--- firstApp/script.js
+++ firstApp/script.js
@@ -1,46 +1,46 @@
 'use strict';
 
- if (4 == 9) {
-    console.log('Bingo!');
- } else {
-    console.log('Error!');
- }
+// const hamburger = true;
+// const fries = true;
 
-///////////////////////////////////////////
+// if (hamburger && fries) {
+//    console.log('Я сыт!');
+// }
 
- const num = 100;
+////////////////////////////////////////////////////
 
- if (num < 49){
-    console.log('Nope!');
- } else if (num > 100) {
-    console.log('Its too much bro!')
- } else {
-    console.log ('You are right!')
- }
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
 
-///////////////////////////////////////////
+// console.log(hamburger === 3 && cola && fries);
 
- const num2 = 51;
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'test');
 
- (num2 === 50) ? console.log ('You are right!') : console.log('Nope!');
+// if (hamburger === 3 && cola === 1 && fries) {
+//    console.log('Все сыты!');
+// } else {
+//    console.log('Мы уходим!');
+// }
 
- ///////////////////////////////////////////
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
 
- const num3 = 51
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+   console.log('Все довольны!');
+} else {
+   console.log('Мы уходим!');
+}
 
- switch (num3) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('Получилось');
-        break;
-    default:
-        console.log('Попробуй еще разок');
-        break;
- }
- 
- 
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+let maxReport, timReport, sashaReport, nickReport = 'done';
+
+console.log(maxReport || timReport || sashaReport || nickReport);
+
+//Задачки 
