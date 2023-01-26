403e4c456c9fb162cd4750e892dea42b8be93bb8
Max <maxlukru@gmail.com>
2023-01-26 11:23:23 +0200

    22. Циклы

--- firstApp/script.js
+++ firstApp/script.js
@@ -24,3 +24,42 @@ for (let i = 1; i < 10; i++) {
     }
     console.log(i);
 }
+//Первое
+for (let i = 1; i <= 10; i++) {
+    
+    console.log(i);
+}
+//Второе
+for (let i = 20; i >= 10; i--) {
+    if (i === 13) {
+        break;
+    }
+  console.log(i);
+}
+//Третье
+for (let i = 2; i <= 10; i++) {
+    if (i === 3) {
+        continue;
+    } if (i === 5) {
+        continue;
+    } if (i === 7) {
+        continue;
+    } if (i === 9) {
+        continue;
+    } 
+ console.log(i);
+}
+//Другой более правильный способ решения 3 задания
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+//Четвертое
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
